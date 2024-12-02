9671f02eed59121fb65eeef9cec3e4e4725b750e
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-02 20:42:35 +0530

    area of square and rectangle
    find area of square and rectangle using JAVA

---
+++ prob4.java
@@ -0,0 +1,11 @@
+/*Write a C program to calculate and print the area of a square and rectangle.
+Side of square is : 5
+Length and breadth of rectanngle is : 4 , 5*/
+public class prob4 {
+    public static void main(String[] args) {
+        // as given
+        int sideOfSquare = 5, lengthOfRectangle = 4, breadthOfRectangle = 5;
+        System.out.println("Area of Square is  : " + (sideOfSquare * sideOfSquare));
+        System.out.println("Area of rectangle is  :" + (lengthOfRectangle * breadthOfRectangle));
+    }
+}

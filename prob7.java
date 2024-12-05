6b663875b9a6d0d2a5572472e4e0cf2ebca69821
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-05 15:17:35 +0530

    previous and next multiple of 10
    if numbers last digit is less than 6 than calculate previous multiple otherwise next multiple without using if-else or loops

--- prob7.java
+++ prob7.java
@@ -3,16 +3,25 @@ number's last digit is less than 5 then find the previous multiple of 10 and if 
 DAILY LAB PRACTICE PROBLEMS
 last digit is greater than or equal to 5 then find the next multiple of 10. [Dont use if else or
 ternary operator ] */
-import java.util.*;
+import java.util.Scanner;
+
 public class prob7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        // Input a two-digit number
         System.out.print("Enter a two-digit number: ");
         int num = scanner.nextInt();
 
-        boolean nextMultiple = (num % 10 > 5) && (System.out.printf("%d of next multiple is %d\n", num, ((num / 10) + 1) * 10) == null);
-        boolean prevMultiple = (num % 10 <= 5) && (System.out.printf("%d of previous multiple is %d\n", num, (num / 10) * 10) == null);
-        scanner.close();;
+        // Compute and print the next or previous multiple of 10
+        System.out.printf(
+            "%d of %s multiple is %d\n", 
+            num, 
+            (num % 10 >= 5) ? "next" : "previous", 
+            (num % 10 >= 5) ? ((num / 10) + 1) * 10 : (num / 10) * 10
+        );
+
+        scanner.close();
     }
 }
+
